84136ac6ff6203903db366aee6355026b3e7e400
Daniel Kolsoi <thadan64@gmail.com>
2019-05-14 13:53:36 -0400

    Initial nginx setup

--- tests/nginx/build.rs
+++ tests/nginx/build.rs
@@ -1,6 +1,8 @@
 #[cfg(target_os = "linux")]
 fn main() {
     println!("cargo:rustc-link-lib=pcre");
+    println!("cargo:rustc-link-lib=crypt");
+    println!("cargo:rustc-link-lib=z");
 }
 
 #[cfg(target_os = "macos")]
