08e4bd4f86142c451e16c2e2ff93cd8cf5328447
fw <fw@immunant.com>
2025-07-10 17:19:50 -0400

    lua, zstd: test in --release mode to allow integer overflow

--- tests/zstd/test.sh
+++ tests/zstd/test.sh
@@ -4,4 +4,4 @@ set -e; set -o pipefail
 SCRIPT_DIR="$(cd "$(dirname "$0" )" && pwd)"
 
 # TODO: run all of `test-fullbench`, compare output to native
-cd "$SCRIPT_DIR/repo" && cargo run --  -i1 2>&1 | tee ../`basename "$0"`.log
+cd "$SCRIPT_DIR/repo" && cargo run --release --  -i1 2>&1 | tee ../`basename "$0"`.log
